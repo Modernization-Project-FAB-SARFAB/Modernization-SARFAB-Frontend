329f79311f20a076f5ebd603d88a6b367091cbaf
CaroEcc <carolinavivianescobarcoca@gmail.com>
2025-03-28 22:06:29 -0400

    Fix sidebar

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -5,7 +5,7 @@ import { ModalProps } from "./Modal.types";
 const Modal: React.FC<ModalProps> = ({ title, isOpen, onClose, children }) => {
     return (
         <Transition appear show={isOpen} as={Fragment}>
-            <Dialog as="div" className="relative z-50" onClose={onClose}>
+            <Dialog as="div" className="relative z-999999" onClose={onClose} >
                 <Transition.Child
                     as={Fragment}
                     enter="ease-out duration-300"
